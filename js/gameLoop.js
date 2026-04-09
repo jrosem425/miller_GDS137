93a38766f7639415d18ecf3877a2574e10d72c7b
jrosem425 <jrmiller425@gmail.com>
2026-04-09 12:41:39 -0400

    color change on collision

--- js/gameLoop.js
+++ js/gameLoop.js
@@ -21,11 +21,13 @@ function animate()
     if(player.x > canvas.width - player.width/2 || player.x < 0 + player.width/2)
     {
         player.vx*=-1
+        player.color = "purple";
     }
 
     if(player.y > canvas.height - player.height/2 || player.y < 0 + player.height/2)
     {
         player.vy*=-1
+        player.color = "darkblue";
     }
     player.draw();
 
